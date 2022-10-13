c4e7fc4e060b029d0602335ecd8e3d8e42df35db
Divya <Divya.kansal@sdettechnologies.com>
2022-10-13 14:45:01 +0530

    First commit

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		String Hungry = "yes";
+		if(Hungry=="yes")
+		{
+			System.out.println("eat something..");
+		}
+		else
+		{
+			System.out.println("dont eat anything.");
+		}
+		
+
+	}
+
+}
