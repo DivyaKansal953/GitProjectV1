4b617ce289bf493a307108ad8630d53a587f5e86
Divya <Divya.kansal@sdettechnologies.com>
2022-10-14 11:22:07 +0530

    NewClass

---
+++ src/Test2.java
@@ -0,0 +1,18 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String Hungry = "yes";
+		if(Hungry=="yes")
+		{
+			System.out.println("eat something..");
+		}
+		else
+		{
+			System.out.println("dont eat anything.");
+		}
+		
+
+	}
+
+}
